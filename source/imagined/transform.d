b431e038e816a90ba8f95b18ba739e143036a70a
dsze <danzap95@gmail.com>
2016-09-22 23:20:38 +0000

    Interference added

--- source/imagined/transform.d
+++ source/imagined/transform.d
@@ -7,6 +7,7 @@ import std.range;
 import std.traits;
 import std.math;
 import std.string;
+import std.stdio;
 
 import dcv.core;
 import dcv.imgproc;
@@ -14,23 +15,27 @@ import imagine.util;
 
 auto filter(Image img, float sensitivity) {
     auto slice = img.sliced;
-    auto avgs = new float[img.channels];
-    for (int i = 0; i < img.channels; i++) {
-
-        auto sl = slice[0..$, 0..$, i];
-        avgs[i] = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
-    } 
-
-    auto avg = avgs.reduce!"a+b"/avgs.length;
-    
+    auto lowlights = slice.slice;
     for (int i = 0; i < img.channels; i++) {
         auto sl = slice[0..$, 0..$, i];
-        avgs[i] = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
-        auto slMask = sl.threshold!ubyte((avgs[i]).to!ubyte);
+	auto avg = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
+        auto slMask = sl.threshold!ubyte((avg).to!ubyte);
 
         sl[] &= slMask;
     }
 
+    for (int i = 0; i < img.channels; i++) {
+	auto low = lowlights[0..$, 0..$, i];
+	auto sl = slice[0..$, 0..$, i];
+	auto avg = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
+        auto slMask = low.threshold!ubyte(0, (avg).to!ubyte);
 
+        low[] &= slMask;
+	auto elem = low.byElement.retro;
+	bringToFront(elem[0 .. $-avg.to!ubyte], elem[$-avg.to!ubyte .. $]);
+	low[] = elem.retro.array.sliced(img.height, img.width);
+    }
+    
+    slice[] += lowlights;
     return slice.asImage(img.format);
 }
