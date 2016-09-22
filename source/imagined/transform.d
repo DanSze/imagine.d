730957200930536e6df92b2a5ad3c4f87f10ca12
dsze <danzap95@gmail.com>
2016-09-22 20:44:45 +0000

    Bitmask application done

--- source/imagined/transform.d
+++ source/imagined/transform.d
@@ -3,23 +3,25 @@ module imagine.transform;
 import std.algorithm;
 import std.array;
 import std.conv;
-import std.experimental.ndslice;
 import std.range;
 import std.traits;
+import std.math;
 import std.string;
 
 import dcv.core;
 import dcv.imgproc;
 import imagine.util;
 
+const blockWidth = 50;
+
 auto binarize(Image img, float sensitivity) {
-    ulong divisor = floor(img.width*img.height*sensitivity).to!ulong;
-    auto slice = img.sliced;
+    auto iSlice = img.sliced;
     for (int i = 0; i < img.channels; i++) {
-        auto sl = slice[0..$, 0..$, i];
-        auto avg = min(255, size_t(0).ndReduce!"a + b"(sl)/divisor);
-        sl[] = sl.threshold!ubyte(avg.to!ubyte)[];    
+        auto sl = iSlice[0..$, 0..$, i];
+        auto avg = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
+        auto slMask = sl.threshold!ubyte((avg).to!ubyte);
+	sl[] &= slMask;
     }
 
-    return slice.asImage(img.format);
+    return iSlice.asImage(img.format);
 }
