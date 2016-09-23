96e3fefda938e447dac0030e361aba5272f47c15
Dan Zapornikov <danzap95@gmail.com>
2016-09-23 01:01:04 -0400

    Genetic rewrite

--- source/imagined/transform.d
+++ source/imagined/transform.d
@@ -13,29 +13,35 @@ import dcv.core;
 import dcv.imgproc;
 import imagine.util;
 
+const windowSize = 7;
+const hiddenNeurons = 9;
+
 auto improve(Image img, float sensitivity) {
     auto slice = img.sliced;
-    auto lowlights = slice.slice;
-    for (int i = 0; i < img.channels; i++) {
-        auto sl = slice[0..$, 0..$, i];
-        auto avg = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
-        auto slMask = sl.threshold!ubyte((avg).to!ubyte);
-
-        sl[] &= slMask;
-    }
-
-    for (int i = 0; i < img.channels; i++) {
-        auto low = lowlights[0..$, 0..$, i];
-        auto sl = slice[0..$, 0..$, i];
-        auto avg = min(255, sl.byElement.reduce!"a+b"/sl.elementsCount*sensitivity);
-        auto slMask = low.threshold!ubyte(0, (avg).to!ubyte);
-
-        low[] &= slMask;
-        auto elem = low.byElement.retro;
-        bringToFront(elem[0 .. $-avg.to!ubyte], elem[$-avg.to!ubyte .. $]);
-        low[] = elem.retro.array.sliced(img.height, img.width);
-    }
     
-    //slice[] += lowlights;
     return slice.asImage(img.format);
+}
+
+ubyte computeNN(Slice!(2,ubyte) window, Slice!(1, float) params) {
+    auto paramMatrix = params[0..windowSize*windowSize*hiddenNeurons].reshape(windowSize*windowSize, hiddenNeurons);
+    auto paramHidden = params[windowSize*windowSize*hiddenNeurons .. $];
+    return window.byElements * paramMatrix * paramHidden;
+}
+
+auto crossOverParams(Slice!(1, float) params1, Slice!(1, float) params2) {
+    auto crossoverPoint = uniform(0, params.elementsCount);
+    return params1[0..crossoverPoint] ~ params2[crossoverPoint..$];
+}
+
+auto fitness(Slice(2, ubyte) img, Slice!(1, float) params) {
+    auto target = img.windows(windowSize, windowSize)
+            .byElements
+            .map!(a => a[windowSize/2, windowSize/2])
+            .array;
+    auto result = img.windows(windowSize, windowSize)
+            .byElements
+            .map!(a => a.computeNN(params))
+            .array;
+    auto diff = target[] - resutl[];
+    return (diff[] * diff[]).sum;
 }
